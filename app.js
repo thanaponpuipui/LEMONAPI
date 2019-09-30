26dd0f2e163a81edf9ed9d2f2d48fbd79317040c
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-10-01 00:00:27 +0700

    server start listening to request

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyPaser = require('body-parser');
+const cors = require('cors');
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
 const app = express();
+
+app.use(cors())
+app.use(bodyPaser.json());
 
 app.use('/', (req, res, next) => {
     res.send('hello');
