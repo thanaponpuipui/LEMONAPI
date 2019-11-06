fd56cf3297076c27da5c2a9a09d3bc4849efd544
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-11-06 18:06:54 +0700

    authorization middleware sign token to request header

---
+++ middlewares/authorization.js
@@ -0,0 +1,25 @@
+const jwt = require('../utils/jwt');
+
+module.exports = (req, res, next) => {
+    console.log("authCheck")
+    const token = req.header('Authorization');
+    if (!token) {
+        const error = new Error('Not Authenticate.');
+        error.errorCode = 401;
+        throw error;
+    }
+    let decodeToken;
+    try {
+        decodeToken = jwt.verify(token);
+    } catch (err) {
+        err.statusCode = 500;
+        throw err;
+    }
+    if (!decodeToken) {
+        const error = new Error('Not Authenticate.');
+        error.errorCode = 401;
+        throw error;
+    }
+    req.accId = decodeToken.accId;
+    next();
+}
