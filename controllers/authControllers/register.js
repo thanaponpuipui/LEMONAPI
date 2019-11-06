13349631fdd227aa81a2bc21ffc27131c10b5a87
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-11-06 18:04:35 +0700

    register controller for demo worked!

--- controllers/authControllers/register.js
+++ controllers/authControllers/register.js
@@ -1,46 +1,74 @@
 const crypt = require('../../utils/crypt');
 const jwt = require('../../utils/jwt');
 const { userVerifier } = require('../../utils/userVerifier');
+const time = require('../../utils/time');
 
-const register = (db) => async (res, req, next) => {
-    const { username, password } = req.body;
-    const {/* owner info */} = req.body;
+const { registerValidate, ownerInfoValidate } = require('../../validation/tempValidate');
+
+const register = (db) => async (req, res, next) => {
+
+    const { username, password, passwordConfirmed } = req.body;
+    const { firstName, lastName, email } = req.body;    
 
     // validation
-
+    const { error:registerInvalid } = registerValidate({username, password, passwordConfirmed});
+    const { error:ownerInfoInvalid } = ownerInfoValidate({firstName, lastName, email});
     const verify = userVerifier(db);
     // use client for tranx
-    let currentDateAndTime;
-    let client;
+    let currentDateAndTime = time.now();
+    const client = await db.connect();
     try {
+        if (registerInvalid) {
+            console.log(registerInvalid);
+            throw registerInvalid;
+        }
+        if (ownerInfoInvalid) {
+            console.table(ownerInfoInvalid);
+            throw ownerInfoInvalid;
+        }
         // check if username already exist
         const isExist = await verify(username);    
         if (isExist) {
             const err = new Error('username is already used');
-            err.code = 401;
+            err.code = 400;
             throw err;
         }
         // create transaction
-        client = await db.connect();
         await client.query('BEGIN');
-        // insert owner info
-        const ownerId = await client.query('INSERT INTO owner() values($) RETURNING owner_id', []);
         // hash password
         const hash = await crypt.hash(password);
         // insert username and password in database
-        const user = await client.query(
+        const { rows:id } = await client.query(
             `INSERT INTO 
-             lemon_accounts(login_name, password, join_date, membership_id, owner_id)
-             values($1, $2, $3, $4, $5)
-             RETURNING acc_id, login_name`,
-            [username, hash, currentDateAndTime, membership, ownerId]
+             lemon_accounts(login_name, password, join_date, meminfo_id)
+             VALUES($1, $2, $3, $4)
+             RETURNING acc_id`,
+            [username, hash, currentDateAndTime, null]
+        );
+        const { acc_id:accId } = id[0];
+        // owner info
+        await client.query(
+            `INSERT INTO owners(
+             first_name,
+             last_name,
+             email,
+             acc_id)
+             VALUES($1, $2, $3, $4)`,
+             [firstName, lastName, email, accId]
         );
         await client.query("COMMIT");
         // response
-        const token = await jwt.sign(user);
-        res.status(200).json(token);
+        const token = jwt.sign({accId, username});
+        // response data for auth state
+        const resData = {
+            accId,
+            username,
+            token,
+        }
+        res.status(200).json(resData);
     } catch (e) {
         await client.query('ROLLBACK');
+        console.trace(e);
         next(e);
     } finally {
         client.release();
