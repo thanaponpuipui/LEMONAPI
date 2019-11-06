912e45b43db7f714ddc8adcd10dfdb7f8dc865e9
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-11-06 18:05:43 +0700

    add check endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,12 @@ const lemondb = require('../database/lemondb');
 // controllers
 const login = require('../controllers/authControllers/login');
 const register = require('../controllers/authControllers/register');
+const check = require('../controllers/authControllers/check');
 
 router.post('/login', login(lemondb));
-
+// demo plan
 router.post('/register', register(lemondb));
+
+router.get('/check', check);
 
 module.exports = router;
