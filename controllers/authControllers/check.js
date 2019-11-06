3997bdfe326df13f0bb1db144864d4c2285281d7
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-11-06 18:03:38 +0700

    check token when revisited

---
+++ controllers/authControllers/check.js
@@ -0,0 +1,25 @@
+const jwt = require('../../utils/jwt');
+
+const check = (req, res, next) => {
+    const token = req.query.token;
+    let decodeToken;
+    try {
+        decodeToken = jwt.verify(token);
+    } catch (e) {
+        e.errorCode = 401;
+        console.log(e);
+        throw e;
+    }
+
+    const { accId, username } = decodeToken;
+
+    const resData = {
+        accId,
+        username,
+        token,
+    }
+
+    return res.status(200).json(resData);
+}
+
+module.exports = check;
