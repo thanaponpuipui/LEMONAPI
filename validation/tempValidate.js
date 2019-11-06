d9d32c08b41667d623b0d74594926ea524ddcb65
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-11-06 18:08:30 +0700

    validation testing

--- validation/tempValidate.js
+++ validation/tempValidate.js
@@ -1,7 +1,21 @@
 const Joi = require('@hapi/joi');
 // Joi use Object schema
 
-module.exports.login = (body) => {
+module.exports.loginValidation = (data) => {
+  const schema = Joi.object({
+    username: Joi.string()
+      .alphanum()
+      .min(6)
+      .max(20)
+      .required(),
+    password: Joi.string()
+      .alphanum()
+      .min(6)
+      .max(20)
+      .required(),
+  })
+
+  return schema.validate(data);
 }
 
 // register process seperatly
@@ -10,29 +24,60 @@ module.exports.login = (body) => {
 // assign resturant info for (can not be null)
 // contact info input seperate from register form
 
-module.exports.register = (body) => {
-  const schema = Joi.object({
-      username: Joi.string()
-        .alphanum()
-        .min(6)
-        .max(20)
-        .required(),
-      password: Joi.string()
-        .alphanum()
-        .min(6)
-        .max(20)
-        .required(),
-      passwordConfirmed: Joi.ref('password')
-  })
+module.exports.registerValidate = (data) => {
   // register field post body -
   // username, password, include password confirmed
+  const schema = Joi.object({
+    username: Joi.string()
+      .alphanum()
+      .min(6)
+      .max(20)
+      .required(),
+    password: Joi.string()
+      .alphanum()
+      .min(6)
+      .max(20)
+      .required(),
+    passwordConfirmed: Joi.ref('password')
+  })
+
+  return schema.validate(data);
 }
 
-module.exports.assignRest = (body) => {
+module.exports.ownerInfoValidate = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string()
+      .required()
+      .pattern(/\D{3,40}/),
+    lastName: Joi.string()
+      .required()
+      .pattern(/\D{3,40}/),
+    email: Joi.string()
+      .required()
+      .email({ minDomainSegments: 2 })
+  })
+
+  return schema.validate(data);
+}
+
+module.exports.assignRest = (data) => {
   // account id !important
   // all field cannot be null
 }
 
-module.exports.addStaff = (body) => {
-
+module.exports.addStaffValidate = (data) => {
+  const schema = Joi.object({
+    name: Joi.string()
+      .required(),
+    isManager: Joi.boolean(),
+    phoneNo: Joi.string()
+      .trim()
+      .regex(/^[0-9]{9,10}$/)
+      .required(),
+    password: Joi.string()
+      .trim()
+      .alphanum()
+      .min(6)
+      .max(12)
+  })
 }
