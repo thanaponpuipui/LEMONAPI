4ef0a4aa3ac6475ba53955b7edc137524fbe03c3
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-11-06 18:03:06 +0700

    add new route and error handler middleware

--- app.js
+++ app.js
@@ -8,18 +8,30 @@ if (process.env.NODE_ENV !== 'production') {
 
 // routes
 const authRoute = require('./routes/auth');
+const restRoute = require('./routes/resturant');
+const staffRoute = require('./routes/staff');
 
 const app = express();
 
 app.use(cors());
+app.use(bodyPaser.urlencoded({extended:false}))
 app.use(bodyPaser.json());
 
 app.use('/auth', authRoute);
+app.use('/rest', restRoute);
+app.use('/staff', staffRoute);
 
 app.use('/', (req, res, next) => {
     res.send('hello');
 })
 
+app.use((err, req, res, next) => {
+    console.log('error handler')
+    if (!err.errorCode) {
+        err.errorCode = 500;
+    }
+    res.status(err.errorCode).json({message:err.message});
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`server run on port ${process.env.PORT}`);
