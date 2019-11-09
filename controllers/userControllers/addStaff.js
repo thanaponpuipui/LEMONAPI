8b23631eebd18fa24b37e7690fa7ad65e96c4e9f
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-11-10 01:14:40 +0700

    move validation in try

--- controllers/userControllers/addStaff.js
+++ controllers/userControllers/addStaff.js
@@ -2,9 +2,8 @@ const { addStaffValidate } = require('../../validation/tempValidate');
 
 const addStaff = (db) => async (req, res, next) => {
     const { name, isManager, phoneNo, password, restId } = req.body;
-    console.log(name)
-    const { err } = addStaffValidate(req.body);
     try {
+        const { err } = addStaffValidate(req.body);
         if (err) {
             const error = new Error('validation error');
             error.errorCode = '400';
@@ -19,9 +18,7 @@ const addStaff = (db) => async (req, res, next) => {
             queryText = 'INSERT INTO rest_staffs(name, login_password, ismanager, phone_no, rest_id) values($1, $2, $3, $4, $5)'
             insertValues = [name, password, isManager, phoneNo, restId]
         }
-        console.log("a")
         await db.query(queryText, insertValues);
-        console.log("a")
         const resData = {
             message:`${name} added`,
         }
