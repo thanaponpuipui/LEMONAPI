b77af5c9d427aed21432d6fe9a61fc6e595d384a
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-11-15 10:07:04 +0700

    update order

--- src/controllers/orderControllers/addNewOrder.js
+++ src/controllers/orderControllers/addNewOrder.js
@@ -1,20 +1,25 @@
-const { addOrderValidation } = require('../../validation/tempValidate');
+const { newOrder } = require('../../models/orderTransaction/newOrder.trx');
+//const { addOrderValidation } = require('../../validation/tempValidate');
 /* 
 * @ typeDef {pool Object from pg database postgresql driver} databasePool
 * @ param {databasePool} db
 */
-const addNewOrder = (db) => async (req, res, next) => {
-    const accId = req.accId;
-    const { restId, foodOrder } = req.body;
+const addNewOrder = async (req, res, next) => {
     try {
+        const { restId, items, orderType } = req.body;
         //validation here
-
-
+        const orderDetail = {
+            orderType,
+            items
+        }
+         await newOrder(orderDetail, restId);
+        const resData = {
+            message: 'successfully add new order!'
+        }
+        res.status(200).json(resData);
     } catch (e) {
         next(e);
     }
-
-    res.json({accId:accId, message:'it work!'})
 }
 
 module.exports = addNewOrder;
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,12 +1,15 @@
 const route = require('express').Router();
-const lemondb = require('../database/lemondb');
-
 const authCheck = require('../middlewares/authorization');
-
 const getOrderCon = require('../controllers/getConPath')('order');
 
 const addNewOrder = require(getOrderCon('addNewOrder'));
+const getAllCurrentOrder = require(getOrderCon('getAllCurrentOrder'));
+const orderCheckout = require(getOrderCon('orderCheckout'));
 
-route.post('/', authCheck, addNewOrder(lemondb));
+route.post('/', authCheck, addNewOrder);
+
+route.get('/', authCheck, getAllCurrentOrder);
+
+route.patch('/:id', authCheck, orderCheckout);
 
 module.exports = route;
