82db51dd6a39b421a31e2018a4348e0dddcea51f
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-11-26 16:02:35 +0700

    change response structure and error pattern

--- src/controllers/userControllers/getAllStaffs.js
+++ src/controllers/userControllers/getAllStaffs.js
@@ -3,20 +3,30 @@ const getAllStaffs = (db) => async (req, res, next) => {
     
     try {
         const query = {
-            text: "SELECT staff_id as staffId, name, avatar, ismanager as isManager FROM rest_staffs WHERE rest_id = $1",
+            text: `SELECT 
+                        staff_id as staffId,
+                        name,
+                        avatar,
+                        ismanager as isManager
+                    FROM rest_staffs
+                    WHERE rest_id = $1`,
             values: [restId],
         }
         const { rows } = await db.query(query);
-        if (rows.length < 1) {
-            const error = new Error('0 staff');
-            error.errorCode = 400;
-            throw error;
+        let message;
+        if (rows.length <= 0) {
+            message = 'no staff found!'
         }
         const staffList = [];
         rows.foreach(row => {
             staffList.push(row);
         })
-        res.status(200).json(staffList);
+        const resData = {
+            flag: 'success',
+            message,
+            data: staffList,
+        }
+        res.status(200).json(resData);
     } catch (e) {
         next(e);
     }
