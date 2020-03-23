f7982918d93f3add7a098bb30a25dab0bb230872
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-03-23 15:54:13 +0700

    fixed sql

--- src/utils/userVerifier.js
+++ src/utils/userVerifier.js
@@ -1,6 +1,6 @@
 module.exports.userVerifier = db => username => {
   const query = {
-    text: 'SELECT * FROM lemon_accounts where login_name = $1',
+    text: 'SELECT * FROM accounts where login_name = $1',
     values: [username],
   };
   return db.query(query).then(({ rows }) => {
